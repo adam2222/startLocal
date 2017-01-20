5e135d3d27721d10b0e43bd12596c63a771d4593
Adam M. Intrator <adam.intra2r@gmail.com>
2017-01-20 18:58:50 -0500

    search includes images of representatives

--- app/components/Location.jsx
+++ app/components/Location.jsx
@@ -21,13 +21,15 @@ const LocationComponent = (props) => {
       <input type="submit" value="Get info for your location" />
     </form>
     <br></br>
-    <table className="striped">
+    <table className="striped z-depth-2">
       <thead>
         <tr>
           <th data-field="office">Official</th>
+          <th data-field="office"></th>
           <th data-field="name">Office Holder</th>
-          <th data-field="Twitter">Twitter</th>
+          <th data-field="Party">Party</th>
           <th data-field="Telephone">Telephone</th>
+          <th data-field="Homepage">Homepage</th>
           <th data-field="Address">Address</th>
         </tr>
       </thead>
@@ -37,7 +39,74 @@ const LocationComponent = (props) => {
           <tr key={idx}>
             <td>{office}</td>
             <td>{officeHolders[idx].name}</td>
+            <td>{ officeHolders[idx].photoUrl &&
+                <img src={`${officeHolders[idx].photoUrl}`}></img>
+              }
+            </td>
             <td>{officeHolders[idx].party}</td>
+            <td>
+              <ul className="collection">
+                {officeHolders[idx].phones.map((phone, idx) => {
+                  return (
+                    <li key={idx}>
+                      {phone}
+                    </li>
+                  )
+                })}
+
+              </ul>
+            </td>
+            <td>
+              <ul className="collection">
+                {officeHolders[idx].channels && officeHolders[idx].channels.map((channel, idx) => {
+                  return (
+                    <li key={idx}>
+                      {channel.type}: {channel.id}
+                    </li>
+                  )
+                })}
+              </ul>
+            </td>
+            <td>
+              <ul className="collection">
+                {officeHolders[idx].urls && officeHolders[idx].urls.map((url, idx) => {
+                  return (
+                    <li key={idx}>
+                      {url}
+                    </li>
+                  )
+                })}
+              </ul>
+            </td>
+            <td>
+              <table>
+                {officeHolders[idx].address && officeHolders[idx].address.map((address, idx) => {
+                  return (
+                    <tr key={idx} >
+                      <ul className="collection">
+                            <li>
+                              {address.line1}
+                            </li>
+                            <li>
+                              {address.line2}
+                            </li>
+                            <li>
+                              {address.city}
+                            </li>
+                            <li>
+                              {address.state}
+                            </li>
+                            <li>
+                              {address.zip}
+                            </li>
+                      </ul>
+                    </tr>
+                  )
+                })}
+
+              </table>
+            </td>
+
           </tr>
         )
       })}
